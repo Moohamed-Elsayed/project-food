e81c19fcbe2b55e5acbdb6c0834449030fc41daf
Mohamed Elsayed <mohamed.elsayed.mmre@gmail.com>
2020-04-19 21:46:07 +0300

    deploy project

---
+++ lib/model/Food.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Food {
+  String id;
+  String name;
+  String image;
+  double price;
+  List subIngredients = [];
+  Timestamp createdAt;
+  Timestamp updateAt;
+
+  Food();
+  Food.fromMap(Map<dynamic, dynamic> data) {
+    id = data['id'];
+    name = data['name'];
+    price = data['price'];
+    image = data['image'];
+    subIngredients = data['subIngredients'];
+    createdAt = data['createdAt'];
+    updateAt = data['updateAt'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'price': price ?? 0.0,
+      'image': image,
+      'subIngredients': subIngredients,
+      'createdAt': createdAt,
+      'updateAt': updateAt
+    };
+  }
+}
